5c000e3f4c2cef993a4fb1a5fc4a375e7d15820c
Michael Ford <mtford@gmail.com>
2014-12-12 21:07:41 -0500

    Task colours

--- scripts/routes/tasks/Task.jsx
+++ scripts/routes/tasks/Task.jsx
@@ -1,11 +1,18 @@
 var React = require('react'),
-    _ = require('underscore');
+    _ = require('underscore'),
+    reflux = require('reflux'),
+    colourFlux = require('../../flux/colours');
 
+// TODO: Once ReactJS has the ability to perform inline hover styles, we can get rid of the awkward mouseout/mouseover handlers
 var Task = React.createClass({
+    mixins: [reflux.ListenerMixin],
     render: function () {
         var onCancel = this.props.onCancel ? _.partial(this.props.onCancel, this) : undefined;
+        var style = this.state.hover ? {borderColor: this.state.color} : {};
+        console.log('style', style);
+        var self = this;
         return (
-            <div className="task">
+            <div className="task" onMouseOver={self.onMouseOver} onMouseOut={self.onMouseOut} style={style}>
                 <span className="title">
                     {this.props.title}
                 </span>
@@ -16,6 +23,34 @@ var Task = React.createClass({
                 </div>
             </div>
         )
+    },
+    componentDidMount: function () {
+        this.cancelListen = this.listenTo(colourFlux.store, function (payload) {
+            this.setState({
+                color: payload.primary
+            });
+        });
+    },
+    componentDidUnmount: function () {
+        this.cancelListen();
+    },
+    onMouseOver: function () {
+        console.log('mouseover')
+        this.setState({
+            hover: true
+        })
+    },
+    onMouseOut: function () {
+        console.log('mouseout')
+        this.setState({
+            hover: false
+        })
+    },
+    getInitialState: function () {
+        return {
+            color: colourFlux.store.getOptions().primary,
+            hover: false
+        }
     }
 });
 
