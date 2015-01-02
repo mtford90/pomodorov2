3cdb82165ae621ae305c30bddfc207c338e74c96
Michael Ford <mtford@gmail.com>
2015-01-02 20:52:42 +0000

    cordova builds

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,10 @@ var gulp = require('gulp'),
     webpack = require('webpack'),
     conf = require('./dev.config'),
     livereload = require('gulp-livereload'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    fs = require('fs-extra'),
+    path = require('path'),
+    cordova = require('cordova-lib').cordova.raw;
 
 
 var JS_FILES = (function () {
@@ -85,29 +88,83 @@ gulp.task('test-bundle', function () {
         .pipe(gulp.dest(publicDest));
 });
 
-gulp.task('compile', function () {
-    var webpackConf = require('./webpack.config.js');
-    delete webpackConf.devtool;
-    // The webpack uglify plugin will uglify both the JS and the embedded styles.
-    var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-    webpackConf.plugins.push(new UglifyJsPlugin());
-    // Ensure that dev-only styles are not applied in the compiled application. By default these styles
-    // apply css transitions to everything, so that hot replacements look awesome ;)
-    webpackConf.plugins.push(new webpack.DefinePlugin({
-        dev: 'false'
-    }));
-    webpackConf.output.filename = conf.compilation.name;
-    var dest = conf.compilation.dir;
-    var publicDest = dest + '/public/';
-    // Pack the JSX & transform into JS.
-    gulp.src(conf.scripts + '/index.jsx')
-        .pipe(gulpWebpack(webpackConf))
-        .pipe(gulp.dest(publicDest));
-    // Rename bundle.js to configured name.
-    gulp.src(HTML_FILES)
-        .pipe(replace('http://localhost:' + conf.webPack.port + '/scripts/bundle.js', conf.compilation.name))
-        .pipe(gulp.dest(publicDest));
+var build = function (uglify) {
+    return function () {
+        var webpackConf = require('./webpack.config.js'),
+            dest = conf.compilation.dir,
+            buildDir = dest + '/public/',
+            cordovaBuildDir = './app/www';
+
+        // No need for sourcemaps for compiled app.
+        delete webpackConf.devtool;
+        // The webpack uglify plugin will uglify both the JS and the embedded styles.
+        if (uglify) {
+            var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+            webpackConf.plugins.push(new UglifyJsPlugin());
+        }
+        // Ensure that dev-only styles are not applied in the compiled application. By default these styles
+        // apply css transitions to everything, so that hot replacements look awesome ;)
+        webpackConf.plugins.push(new webpack.DefinePlugin({
+            dev: 'false'
+        }));
+        webpackConf.output.filename = conf.compilation.name;
+
+        // Pack the JSX & transform into JS.
+        gulp.src(conf.scripts + '/index.jsx')
+            .pipe(gulpWebpack(webpackConf))
+            .pipe(gulp.dest(buildDir));
+        // Rename bundle.js to configured name.
+        gulp.src(HTML_FILES)
+            .pipe(replace('http://localhost:' + conf.webPack.port + '/scripts/bundle.js', conf.compilation.name))
+            .pipe(gulp.dest(buildDir));
+
+        gulp.src('./img/**/*', {base: '.'})
+            .pipe(gulp.dest(buildDir));
+
+        gulp.src('./vendor/**/*', {base: '.'})
+            .pipe(gulp.dest(buildDir));
+
+        // www === bin/public
+        fs.symlinkSync(buildDir, cordovaBuildDir);
+
+        return cordova.plugins('add', plugins)
+            .then(function () {
+                // point to node_modules/cordova-android/
+                return cordova.platform('add', platform_dirs);
+            });
+    };
+};
+
+gulp.task('cordova-clean', function () {
+    fs.removeSync('app');
 });
+
+// Initial setup for cordova app.
+gulp.task('cordova-create', ['cordova-clean'], function () {
+    var buildDir = 'app';
+    fs.mkdirSync(buildDir);
+    process.chdir(buildDir);
+    fs.symlinkSync(path.join('..', 'CordovaConfig.xml'), 'config.xml');
+    fs.symlinkSync(path.join('..', 'bin/public'), 'www');
+    return cordova.plugins('add', ['org.apache.cordova.file'])
+        .then(function () {
+            // point to node_modules/cordova-android/
+            var android = path.join('..', 'node_modules/cordova-android'),
+                browser = path.join('..', 'node_modules/cordova-browser');
+            return cordova.platform('add', [android, browser]) ;
+        });
+});
+
+gulp.task('cordova-build', function () {
+    var buildDir = 'app';
+    process.chdir(buildDir);
+    return cordova.build()
+        .then(function () {
+            fs.copySync('platforms/android/ant-build/CordovaApp-debug.apk', path.join('..', 'bin/TomatoSoup-debug.apk'));
+        });
+});
+
+gulp.task('compile', build(true));
 
 gulp.task('sass-landing', function () {
     gulp.src('./landing/scss/main.scss')
